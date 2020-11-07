119196f3e3172976ebc04c8e653b16b82fb3edb5
John Paul Manoza <johnpaul@bigdish.com>
2020-11-07 10:35:58 +0800

    Initial Commit

---
+++ CurrencyConvert/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  CurrencyConvert
+//
+//  Created by John Paul Manoza on 11/7/20.
+//  Copyright © 2020 John Paul Manoza. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
